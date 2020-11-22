eeec270a04117350a69dcfca24d3bc25c6e01d4a
Kingsyard901 <henrik.ekelund@outlook.com>
2020-11-22 20:01:24 +0100

    Final update
    Small webscraper to scrape my website norrala consulting for links.

--- scrape.js
+++ scrape.js
@@ -1,8 +1,25 @@
 const request = require('request');
 const cheerio = require('cheerio');
+const fs = require('fs');
+
+const writeStream = fs.createWriteStream('website_links.csv');
+
+//Write Headers
+writeStream.write('Data scraped from the Website \n');
 
 request('https://norralaconsulting.se/', (error, response, html) => {
     if (!error && response.statusCode == 200) {
         const $ = cheerio.load(html);
+
+        $('a').each((i, el) => {
+            const link = $(el)
+            .attr('href');
+
+            // Write Row To CSV
+            writeStream.write(`${link} \n`);
+
+            console.log(link);
+        })
+        console.log('Scraping data done...');
     }
 });
